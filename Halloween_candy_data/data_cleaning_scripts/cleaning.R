d2d0377477cabf7e54ddb13e310c25abc486626a
Naomi Penfold <9406530+npscience@users.noreply.github.com>
2023-06-13 17:15:25 +0100

    Add clean 2016 data
    And keep both long and wide formats as new csv files, to use in analyses

--- Halloween_candy_data/data_cleaning_scripts/cleaning.R
+++ Halloween_candy_data/data_cleaning_scripts/cleaning.R
@@ -10,7 +10,7 @@ x2015 <- read_excel("raw_data/boing-boing-candy-2015.xlsx", sheet = "Form Respon
 x2016 <- read_excel("raw_data/boing-boing-candy-2016.xlsx", sheet = "Form Responses 1")
 x2017 <- read_excel("raw_data/boing-boing-candy-2017.xlsx", sheet = "responses (2) (1).csv")
 
-# 2015 data: subset and make long format
+# Tidy 2015 data ---------
 
 ## step 1: subset to remove unwanted columns
 x2015_subset <- x2015 %>% 
@@ -50,8 +50,9 @@ x2015_subset_converted <- x2015_subset %>%
          year = as.numeric(format(year, "%Y"))) %>% 
   # make empty columns for gender and country (missing in this year's dataset)
   mutate(gender = rep(NA_character_, nrow(x2015_subset)), .after = age) %>% 
-  mutate(country = rep(NA_character_, nrow(x2015_subset)), .after = gender) 
-# expect dim: 5630 observations x 85 variables
+  mutate(country = rep(NA_character_, nrow(x2015_subset)), .after = gender) %>% 
+  filter(!id == 1573) # remove 1 duplicate
+# expect dim: 5629 observations x 85 variables
 # cols 1:6 are rater_info, 7-85 are candy items
 
 ## Notes: 
@@ -63,16 +64,55 @@ x2015_subset_converted <- x2015_subset %>%
 
 ## step 3: make long format tidy data
 # with one column for candy items [7-85], one column for rating
-# expect 5630 x 85 df to become 
-# (5630 raters * 79 items) x 8 = 444,770 x 8
+# expect 5629 x 85 df to become 
+# (5629 raters * 79 items) x 8 = 444,691 x 8
 x2015_tidy <- x2015_subset_converted %>% 
   pivot_longer(cols = -c(year, id, age, gender, country, goes_trick_or_treating),
                names_to = "candy_item",
                values_to = "rating")
 
-dim(x2015_tidy) # 444,770 obs x 8 var - as expected
-head(x2015_tidy, n = 20)
-tail(x2015_tidy, n = 20)
+# step 4: write tidied 2015 data to new csv file
+write_csv(x2015_subset_converted, "clean_data/2015_clean_wide.csv")
+write_csv(x2015_tidy, "clean_data/2015_clean_long.csv")
 
+# Tidy 2016 data ---------
 
-# 2016 data: subset and make long format
+## step 1: subset to remove unwanted columns
+x2016_subset <- x2016 %>% 
+  select(1:11,13,14,16:20,23:25,28:30,33:37,39:42,44:48,50:68,
+         70:78,80:89,91:101,103,106) 
+# not yet including 107:109 for additional ratings and comments
+# 1259 obs x 89 var
+
+## step 2: clean up included variables to produce required common df structure: 
+# [1]id [2]year [3]age [4]gender [5]country 
+# [6]goes_trick_or_treating [7]candy_item [8]rating
+x2016_subset_converted <- x2016_subset %>% 
+  # make an id column to retain unique identifier for each person
+  mutate(id = c(1:nrow(x2016_subset)), .before = "Timestamp") %>% 
+  # rename variables
+  rename(year = "Timestamp",
+         goes_trick_or_treating = "Are you going actually going trick or treating yourself?",
+         gender = "Your gender:",
+         age = "How old are you?",
+         country = "Which country do you live in?"
+  ) %>%
+  # change format of age and year data
+  mutate(age = as.integer(age), # Note: this introduces NAs by coercion
+         year = as.numeric(format(year, "%Y")))
+# outputs df: 1259 obs. x 90 var (1 more col than subset: id)
+
+## step 3: make long format tidy data
+# with one column for candy items 7:90, one column for rating
+# expect 1259 x 90 df to become 
+# (1259 raters * 84 items) x 8 = 105,756 x 8
+x2016_tidy <- x2016_subset_converted %>% 
+  pivot_longer(cols = -c(year, id, age, gender, country, goes_trick_or_treating),
+               names_to = "candy_item",
+               values_to = "rating")
+
+dim(x2016_tidy) # 105,756 x 8
+
+# step 4: write tidied 2016 data to new csv file
+write_csv(x2016_subset_converted, "clean_data/2016_clean_wide.csv")
+write_csv(x2016_tidy, "clean_data/2016_clean_long.csv")
